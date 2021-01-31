150add52f0288a2d0edd6c1dabd375f85c2314da
CustomRoses <luc.aggett@gmail.com>
2021-01-31 15:11:18 +0100

    Added Music Commands to rewrite

--- src/main/java/Flowery/Core.java
+++ src/main/java/Flowery/Core.java
@@ -18,6 +18,7 @@ import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.lifecycle.ReadyEvent;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.VoiceState;
+import discord4j.core.object.entity.Member;
 import discord4j.core.object.entity.User;
 import discord4j.core.object.entity.channel.MessageChannel;
 import discord4j.core.object.entity.channel.VoiceChannel;
@@ -31,7 +32,22 @@ public class Core {
 
 	public static void main(String[] args) {
 
-		
+		// Creates AudioPlayer instances and translates URLs to AudioTrack instances
+		final AudioPlayerManager playerManager = new DefaultAudioPlayerManager();
+
+		// This is an optimization strategy that Discord4J can utilize.
+		// It is not important to understand
+		playerManager.getConfiguration()
+		    .setFrameBufferFactory(NonAllocatingAudioFrameBuffer::new);
+
+		// Allow playerManager to parse remote sources like YouTube links
+		AudioSourceManagers.registerRemoteSources(playerManager);
+
+		// Create an AudioPlayer so Discord4J can receive audio data
+		final AudioPlayer player = playerManager.createPlayer();
+
+		// We will be creating LavaPlayerAudioProvider in the next step
+		AudioProvider provider = new LavaPlayerAudioProvider(player);
 		
 		final GatewayDiscordClient client = DiscordClientBuilder.create(args[0]).build().login().block();
 		client.getEventDispatcher().on(MessageCreateEvent.class)
@@ -43,6 +59,23 @@ public class Core {
 	            .next()))
 	    .subscribe();
 		
+		// Commands that require variables, input, etc
+		
+		commands.put("join", event -> Mono.justOrEmpty(event.getMember())
+			    .flatMap(Member::getVoiceState)
+			    .flatMap(VoiceState::getChannel)
+			    // join returns a VoiceConnection which would be required if we were
+			    // adding disconnection features, but for now we are just ignoring it.
+			    .flatMap(channel -> channel.join(spec -> spec.setProvider(provider)))
+			    .then());
+		
+		final TrackScheduler scheduler = new TrackScheduler(player);
+		commands.put("play", event -> Mono.justOrEmpty(event.getMessage().getContent())
+		    .map(content -> Arrays.asList(content.split(" ")))
+		    .doOnNext(command -> playerManager.loadItem(command.get(1), scheduler))
+		    .then());
+		
+		
 		client.onDisconnect().block();
 		
 
@@ -50,6 +83,8 @@ public class Core {
 
 	private static final Map<String, Command> commands = new HashMap<>();
 	
+	
+	// static commands 
 	static {
 		commands.put("ping",
 				event -> event.getMessage().getChannel().flatMap(channel -> channel.createMessage("<:pinged:747783377322508290>")).then());
